09a95dcd304fdaaa313ea6c4f72790adecb6afda
Dominic Damoah <dominic.imakestuff@gmail.com>
2023-08-25 08:24:34 -0400

    initial site up

---
+++ next stuff/out/_next/static/dp00dDlUUI3noZOjEbIVx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/02ef0a9c5f95a2ff.css","static/chunks/pages/index-672ed71577d765a8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
